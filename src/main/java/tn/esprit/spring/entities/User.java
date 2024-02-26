b61ce784ff35349b96f75f2f25587f7102d8c489
farah-telli <farah.telli@esprit.tn>
2024-02-26 11:18:20 +0100

    modification

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -17,7 +17,7 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
-	  
+	//commentaire modif 
 	//commentaire 4Twin3
 	// commentaitre 3ALINFO5 
 	
